9684204dee7df9443dcfd2d38435970ae0786bc1
Jorge Luiz <jorge.luizchaves@yahoo.com.br>
2013-11-16 10:35:24 -0200

    overrided toString method in Complex class

--- ScalaTutorial/CreatingObjects/Complex.scala
+++ ScalaTutorial/CreatingObjects/Complex.scala
@@ -3,4 +3,6 @@ class Complex(x: Double, y: Double){
  def re = x;
  def mi = y;
 
+ override def toString() =
+ "toString method overrided";
 }
--- ScalaTutorial/CreatingObjects/HiClass.scala
+++ ScalaTutorial/CreatingObjects/HiClass.scala
@@ -2,6 +2,7 @@ object HiClass{
 
  def main(args: Array[String]){
   val c = new Complex(1.2, 3.4);
-  println("the x value is: " + c.re());
+  println("the x value is: " + c.re);
+  println(c.toString());
  }
 }
